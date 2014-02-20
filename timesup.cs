52dd29e9dd4996d27c533932e4c13ce7b2500a43
Frederick Ollinger <fred.ollinger@seescan.com>
2014-02-20 12:25:23 -0800

    Status bar, error checking, and can now reset timer.

--- timesup.cs
+++ timesup.cs
@@ -16,6 +16,8 @@ public class TimesUp : Form
     }
 
     private Button button; 
+    private bool started=false;
+    private StatusBarPanel statusbar;
 	private Popup pop;
 	static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
     private DateTimePicker dtp = new DateTimePicker();
@@ -28,6 +30,7 @@ public class TimesUp : Form
         this.Padding = new System.Windows.Forms.Padding(20);
 
         button = new Button();
+        button.BackColor = System.Drawing.Color.Gray;
         button.Text = "Start Timer";
         button.Location = new Point(30, 70);
         button.Click += new EventHandler(OnClick);
@@ -42,32 +45,60 @@ public class TimesUp : Form
         dtp.Dock = DockStyle.Fill;
         dtp.Parent = this;
 
+        StatusBar statusBar1 = new StatusBar();
+        statusbar = new StatusBarPanel();
+        statusBar1.BackColor = System.Drawing.Color.Gray;
+        statusbar.Text = "Ready...";
+        statusBar1.ShowPanels = true;
+        statusBar1.Panels.Add(statusbar);
+        this.Controls.Add(statusBar1);
+
         /* Adds the event and the event handler for the method that will 
 	  	  *           process the timer event to the timer. */
-				myTimer.Tick += new EventHandler(TimerEventProcessor);
+		myTimer.Tick += new EventHandler(TimerEventProcessor);
 
 
         CenterToScreen();
     }
 
-    void OnClick(object sender, EventArgs e) {
-				//DateTime date1 = DateTime.Now;
-				DateTime chosenTime=dtp.Value;
-				TimeSpan setTime=chosenTime.Subtract(DateTime.Now);
-        button.Text = "Stop";
-				myTimer.Interval = (int) setTime.TotalSeconds * 1000;
-			  Console.WriteLine( "date: " + myTimer.Interval.ToString() );
-				myTimer.Start();
+    void resetTimer() {
+        button.Text = "Start Timer";
+        myTimer.Stop();
+        started=false;
+        return;
     }
 
-		//static void CheckStatus(Object state) {
-		//void CheckStatus(Object state) {
+    void OnClick(object sender, EventArgs e) {
+        if (started){
+            resetTimer(); 
+            return;
+        }
+
+		DateTime chosenTime=dtp.Value;
+		TimeSpan setTime=chosenTime.Subtract(DateTime.Now);
+	    Console.WriteLine( "date: " + myTimer.Interval.ToString() );
+        if (setTime.TotalSeconds < 1){
+            statusbar.Text = "Time Must be in Future";
+            return;
+        }
+        started=true;
+        statusbar.Text = "Timer started.";
+        button.Text = "Stop";
+		myTimer.Interval = (int) setTime.TotalSeconds * 1000;
+		myTimer.Start();
+    }
+
     private void TimerEventProcessor(Object myObject, EventArgs myEventArgs) {
-			  myTimer.Stop();
-				Console.WriteLine("Timer Action!!");
+        resetTimer();
+        /*
+        myTimer.Stop();
+        started=false;
+	    Console.WriteLine("Timer Action!!");
         button.Text = "Start Timer";
-				pop = new Popup();
-				pop.Show();
+        */
+
+		pop = new Popup();
+		pop.Show();
      }
 
 } // END public class TimesUp : Form
