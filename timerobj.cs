65273a474b46b17f4320ed2a87328d9ce0fade54
Frederick Ollinger <fred.ollinger@seescan.com>
2014-04-15 12:14:43 -0700

    Added stub for handling the extend timer button pressed.

--- timerobj.cs
+++ timerobj.cs
@@ -5,8 +5,8 @@ using System.Windows.Forms;
 
 namespace TimerNS{
 public class TimerObj : System.Windows.Forms.Timer {
-		private String text="Times Up";
-		public delegate void ChangedEventHandler(object sender, EventArgs e);
+	private String text="Times Up";
+	public delegate void ChangedEventHandler(object sender, EventArgs e);
   	public event ChangedEventHandler TimerDeleted;
 
     public TimerObj(){
@@ -18,27 +18,35 @@ public class TimerObj : System.Windows.Forms.Timer {
 				TimerDeleted(this, e);
 		}
 
-    public void setText(String str){
-       text=str; 
+        public void setText(String str){
+            text=str; 
 		}
 
-    private void TimerEventProcessor(Object myObject, EventArgs myEventArgs) {
-        Stop();
-				showtimesup();
-    }
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs) {
+            Stop();
+			showtimesup();
+        }
 
 		private TimerPopup pop;
-    public void showtimesup() {
-		    pop = new TimerPopup(text);
-        pop.BackColor = System.Drawing.Color.Red;
-        pop.buttonDelete.Click += new EventHandler(HandleDelete);
-		    pop.Show();
-    }
+            public void showtimesup() {
+	        pop = new TimerPopup(text);
+            pop.BackColor = System.Drawing.Color.Red;
+            pop.buttonDelete.Click += new EventHandler(HandleDelete);
+            pop.buttonExtend.Click += new EventHandler(HandleExtend);
+	        pop.Show();
+        }
 
     void HandleDelete(object sender, EventArgs e) {
-				Console.WriteLine( "Delete clicked");
-				OnTimerDeleted(EventArgs.Empty);
-				pop.Close();
+		Console.WriteLine( "Delete clicked");
+        // todo need to send out which object is deleted, I think
+		OnTimerDeleted(EventArgs.Empty); // shoot out a signal telling that we are deleted
+		pop.Close();
+    }
+
+    void HandleExtend(object sender, EventArgs e) {
+		Console.WriteLine( "Extend clicked");
+        // todo need to restart the timer with new time
+		pop.Close();
     }
 }
 } // namespace TimerNS
