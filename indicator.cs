41557159a9b41094cfafcc3c14294393e123c575
Fred Ollinger <follinge@gmail.com>
2015-04-04 21:25:11 -0700

    Stub to build the indicator added.

--- indicator.cs
+++ indicator.cs
@@ -4,9 +4,8 @@
 
 using AppIndicator;
 using System;
-using System.Windows;
-using System.Drawing;
-using System.Windows.Forms;
+using Gtk;
+using Gdk;
 
 public partial class TimesIndicator {
 private String _ExecutableFolder = "";
@@ -18,5 +17,39 @@ private String ExecutableFolder {
     }
 
 } // END ExecutableFolder 
+
+private void BuildMenu() {
+    ApplicationIndicator indicator = new ApplicationIndicator (
+        "sample-application", 		//id of the the indicator icon
+	"app-icon",			        //file name of the icon (will look for app-icon.png) 
+	Category.ApplicationStatus, 
+	ExecutableFolder            //the folder where to look for app-icon.png
+    );	// END ApplicationIndicator
+	
+/*
+        //Build Popup Menu for ApplicationIndicator
+        Menu popupMenu = new Menu ();
+    
+        //Show menu item
+        ImageMenuItem menuItemShow = new ImageMenuItem ("Show");
+        menuItemShow.Image = new Gtk.Image(Stock.Info, IconSize.Menu);
+        menuItemShow.Activated += (sender, e) => this.Visible = !this.Visible;
+        popupMenu.Append(menuItemShow);
+    
+        popupMenu.Append(new SeparatorMenuItem());
+    
+        //Quit menu item
+        ImageMenuItem menuItemQuit = new ImageMenuItem ("Quit");
+        menuItemQuit.Image = new Gtk.Image (Stock.Quit, IconSize.Menu);
+        menuItemQuit.Activated += (sender, e) => Application.Quit ();
+        popupMenu.Append (menuItemQuit);
+    	
+        popupMenu.ShowAll();
+    		
+        //Assign menu and make indicator active
+        indicator.Menu = popupMenu;
+        indicator.Status = AppIndicator.Status.Active;	
+*/
+    } // BuildMenu()
 
 } // END TimesIndicator
